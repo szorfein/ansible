58f1fd65aea8cb6bedf264c85957a6ba0b1b9d7f
szorfein <szorfein@protonmail.com>
2021-12-31 08:41:24 -0500

    install.sh - separate --client --server

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@
 set -o errexit -o nounset
 
 AUTH="sudo"
+NEWUSER="veil"
 HARDEN=false
+SERVER=false
+CLIENT=false
+INSTALL=false
 
 red="\033[1;31m"
 green="\033[1;32m"
@@ -20,8 +24,9 @@ msg_ok() { echo "${green}[${white}+${green}]${white} $1${end}"; }
 
 die() { echo "${red}[${white}-${red}]${white} $1 ${end}"; exit 1; }
 
-dep_for_void() {
-  sudo xbps-install ansible openssh
+
+installing() {
+  "$AUTH" "$INSTALL" $@
   if [ "$?" -eq 0 ] ; then
     msg_ok "Dependencies installed."
   else
@@ -29,7 +34,13 @@ dep_for_void() {
   fi
 }
 
-config_for_void() {
+dep_for_void() {
+  INSTALL="xbps-install"
+  if "$SERVER" ; then installing "openssh" ; fi
+  if "$CLIENT" ; then installing "ansible openssh" ; fi
+}
+
+configuration() {
   [ -f /etc/ssh/ssh_config_backup ] \
     || "$AUTH" mv /etc/ssh/ssh_config /etc/ssh/ssh_config_backup
 
@@ -47,20 +58,20 @@ config_for_void() {
 }
 
 new_group() {
-  if ! grep -q ssh_user /etc/group ; then
-    "$AUTH" groupadd ssh_user
+  if ! grep -q ssh-user /etc/group ; then
+    "$AUTH" groupadd ssh-user
     if [ "$?" -eq 0 ] ; then
-      msg_ok "Group ssh_user created."
+      msg_ok "Group ssh-user created."
     else
-      die "Can't create the group ssh_user."
+      die "Can't create the group ssh-user."
     fi
   fi
 }
 
 add_to_group() {
-  "$AUTH" usermod -a -G ssh_user "$USER"
+  "$AUTH" usermod -a -G ssh-user "$NEWUSER"
   if [ "$?" -eq 0 ] ; then
-    msg_ok "User $USER added."
+    msg_ok "User $NEWUSER added."
   else
     die "Error Add_to_group"
   fi
@@ -87,11 +98,11 @@ create_server_keys() {
 }
 
 create_client_keys() {
-  [ -f "$HOME"/.ssh/ansible_rsa.key ] \
-    || /usr/bin/ssh-keygen -t rsa -b 4096 -o -a 100 -f "$HOME"/.ssh/ansible_rsa.key
+  [ -f /home/"$1"/.ssh/ansible_rsa.key ] \
+    || "$AUTH" -u "$1" /usr/bin/ssh-keygen -t rsa -b 4096 -o -a 100 -f /home/"$1"/.ssh/ansible_rsa.key
 
-  [ -f "$HOME"/.ssh/ansible_ed25519.key ] \
-    || /usr/bin/ssh-keygen -t ed25519 -o -a 100 -f "$HOME"/.ssh/ansible_ed25519.key
+  [ -f /home/"$1"/.ssh/ansible_ed25519.key ] \
+    || "$AUTH" -u "$1" /usr/bin/ssh-keygen -t ed25519 -o -a 100 -f /home/"$1"/.ssh/ansible_ed25519.key
 }
 
 service_for_void() {
@@ -118,17 +129,69 @@ hardening_void() {
   "$AUTH" sv restart sshd
 
   msg_ok "Service sshd hardened."
-  quote "If it doesn't work, you need to logout and back."
 
+  exit 0
+}
+
+show_options() {
+  echo "$0 - Install dependencies for start with Ansible."
+  echo "---------------------"
+  echo "Options:"
+  echo " -s|--server, install server side."
+  echo " -c|--client, install client side."
+  echo " --hardening, use it on the server after a successfully connection."
   exit 0
 }
 
 options() {
   case "$1" in
     --hardening) HARDEN=true; shift ;;
-    -h | --help) echo "Use '$0 --hardening' after a successfull connection."; exit 0; ;;
+    -s|--server) SERVER=true; shift ;;
+    -c|--client) CLIENT=true; shift ;;
+    -h | --help) show_options ;;
     *) die "Invalid argument: $1" ;;
   esac
+}
+
+new_user() {
+  if ! grep -q "$NEWUSER" /etc/passwd ; then
+    "$AUTH" useradd -m "$NEWUSER"
+    if [ "$?" -eq 0 ] ; then
+      msg_ok "User $NEWUSER created."
+    else
+      die "Err service"
+    fi
+  fi
+}
+
+new_user_sudo() {
+  "$AUTH" cp ./deps/sudo /etc/sudoers.d/"$NEWUSER"
+  "$AUTH" chmod 0400 /etc/sudoers.d/"$NEWUSER"
+}
+
+void_server() {
+  title "Creating a new user $NEWUSER..."
+  new_user
+  title "Creating a new group ssh-user..."
+  new_group
+  title "Adding $NEWUSER to group ssh-user..."
+  add_to_group
+  title "Adding sudo permission..."
+  new_user_sudo
+  title "Creating keys for the server..."
+  create_server_keys
+  title "Creating keys for $NEWUSER..."
+  create_client_keys "$NEWUSER"
+  title "Service..."
+  service_for_void
+  msg_ok "Server configured, gl."
+}
+
+void_client() {
+  create_client_keys "$USER"
+  quote "Copy your keys with e.g:"
+  quote "ssh-copy-id localhost"
+  quote "Connect with -> ssh -i ~/.ssh/ansible_ed25519.key $NEWUSER@localhost"
 }
 
 main() {
@@ -141,19 +204,9 @@ main() {
     title "Installing..."
     dep_for_void
     title "Configuring..."
-    config_for_void
-    title "Creating a new group ssh_user..."
-    new_group
-    title "Adding the current user $USER to ssh_user..."
-    add_to_group
-    title "Creating keys..."
-    create_server_keys
-    create_client_keys
-    title "Service..."
-    service_for_void
-    quote "Copy your keys with e.g:"
-    quote "ssh-copy-id localhost"
-    quote "Connect with -> ssh -i ~/.ssh/ansible_ed25519.key localhost"
+    configuration
+    if "$SERVER" ; then void_server ; fi
+    if "$CLIENT" ; then void_client ; fi
     msg_ok "Ended, gl."
   else
     die "Your system is not yet supported :("
